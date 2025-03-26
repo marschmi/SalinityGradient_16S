c0360d536b86fe2fe3744c55c02ca46f986da482
Marian Schmidt <marschmi@cornell.edu>
2025-03-26 15:30:59 -0400

    halfway through lab today

---
+++ code/colors.R
@@ -0,0 +1,7 @@
+# Setting colors for stations 
+station_colors <- c(
+  "Shipping Channel" = "dodgerblue4",
+  "Aransas Bay" = "dodgerblue2",
+  "Copano West" = "#D9CC3C",
+  "Copano East" = "#A0E0BA",
+  "Mesquite Bay" = "#00ADA7")
