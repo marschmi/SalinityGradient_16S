bd627ccef9c5dc1009775341983cc4160c086304
Marian Schmidt <marschmi@cornell.edu>
2025-04-17 09:51:24 -0400

    updating typo in interpretation #3

--- code/colors.R
+++ code/colors.R
@@ -5,3 +5,33 @@ station_colors <- c(
   "Copano West" = "#D9CC3C",
   "Copano East" = "#A0E0BA",
   "Mesquite Bay" = "#00ADA7")
+
+
+
+# Set the phylum colors
+phylum_colors <- c(
+  Acidobacteriota = "navy", 
+  Actinomycetota = "darkslategray2", 
+  Armatimonadota = "deeppink1",
+  Alphaproteobacteria = "plum2", 
+  Bacteroidota = "gold", 
+  Betaproteobacteria = "plum1", 
+  Bdellovibrionota = "red1",
+  Chloroflexi="black", 
+  "Candidatus Kapabacteria" = "firebrick",
+  Cyanobacteriota = "limegreen",
+  Deltaproteobacteria = "grey", 
+  Desulfobacterota="magenta",
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "greenyellow",
+  "Marinimicrobia (SAR406 clade)" = "yellow",
+  Myxococcota = "#B5D6AA",
+  Pseudomonadota = "royalblue",
+  Planctomycetota = "darkorange", 
+  "SAR324 clade(Marine group B)" = "olivedrab",
+  Proteobacteria_unclassified = "greenyellow",
+  Thermoplasmatota = "green",
+  Thermoproteota = "palevioletred1",
+  Verrucomicrobiota = "darkorchid1",
+  Other = "grey")
+
