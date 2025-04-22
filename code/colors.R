25e33e71bd638eb50fc86bb168b0daef5bf6026d
Marian Schmidt <marschmi@cornell.edu>
2025-04-22 17:02:35 -0400

    updates

--- code/colors.R
+++ code/colors.R
@@ -17,7 +17,7 @@ phylum_colors <- c(
   Bacteroidota = "gold", 
   Betaproteobacteria = "plum1", 
   Bdellovibrionota = "red1",
-  Chloroflexi="black", 
+  Chloroflexota="black", 
   "Candidatus Kapabacteria" = "firebrick",
   Cyanobacteriota = "limegreen",
   Deltaproteobacteria = "grey", 
@@ -34,4 +34,36 @@ phylum_colors <- c(
   Thermoproteota = "palevioletred1",
   Verrucomicrobiota = "darkorchid1",
   Other = "grey")
+
+
+
+
+phylum_gus_colors <- c(
+  Acidobacteriota = "navy", 
+  Actinomycetota = "darkslategray2", 
+  Armatimonadota = "deeppink1",
+  Alphaproteobacteria = "plum2", 
+  Bacteroidota = "gold", 
+  Betaproteobacteria = "plum1", 
+  Bdellovibrionota = "red1",
+  Chloroflexota="black", 
+  Crenarchaeota = "yellow",
+  Cyanobacteriota = "limegreen",
+  Deltaproteobacteria = "grey", 
+  Desulfobacterota="magenta",
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "greenyellow",
+  Patescibacteria = "yellow",
+  Myxococcota = "#B5D6AA",
+  #Nitrospirota = "palevioletred1",
+  Pseudomonadota = "palevioletred1",
+  Planctomycetota = "darkorange", 
+  "SAR324 clade(Marine group B)" = "olivedrab",
+  Gemmatimonadota = "greenyellow",
+  Thermodesulfobacteriota = "royalblue",
+  Verrucomicrobiota = "darkorchid1",
+  Other = "grey",
+  Rare = "grey")
+
+
 
